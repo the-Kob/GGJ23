0809057ee0b1f174c6b9b9db6403b15f719c1913
Paulina <paulinajuzwin@gmail.com>
2023-02-05 01:09:05 +0000

    Add an indicator to press "R" to the player

--- Assets/Scripts/Abilities/CooldownIndicator.cs
+++ Assets/Scripts/Abilities/CooldownIndicator.cs
@@ -12,13 +12,21 @@ public class CooldownIndicator : MonoBehaviour
     private RectTransform cooldownBackground;
     private bool playerRooted;
     private TextMeshProUGUI cooldownText;
+    private TextMeshProUGUI abilityKeyText;
+
+    private string storedAbilityKeyText = "";
+    private string cooldownAbilityKeyText = "R";
 
     void Start() {
         RootsController.Instance.RootStart.AddListener(OnPlayerRootStart);
         RootsController.Instance.RootEnd.AddListener(OnPlayerRootEnd);
         cooldownBackground = (RectTransform)transform.Find("Cooldown background");
         cooldownForeground = (RectTransform)transform.Find("Cooldown foreground");
+        
         cooldownText = transform.Find("Cooldown Text").GetComponent<TextMeshProUGUI>();
+        abilityKeyText = transform.Find("Ability Key").GetComponent<TextMeshProUGUI>();
+        storedAbilityKeyText = abilityKeyText.text;
+        
         transform.Find("Ability Key").GetComponent<TextMeshProUGUI>().text = abilitySlot.key.ToString().ToUpper();
 
         cooldownBackground.gameObject.GetComponent<Image>().sprite = abilitySlot.currentAbility.abilityUI;
@@ -30,11 +38,15 @@ public class CooldownIndicator : MonoBehaviour
         if(currentAbilityState == AbilitySlot.AbilityState.ON_COOLDOWN) {
             float percentage = abilitySlot.GetCooldownPercentage();
             cooldownForeground.sizeDelta = new Vector2(cooldownBackground.sizeDelta.x, cooldownBackground.sizeDelta.y*(1-percentage));
+            
             cooldownText.text = ((1-percentage)*abilitySlot.GetTotalCooldown()).ToString("F1");
+            abilityKeyText.text = cooldownAbilityKeyText;
 
         } else if(currentAbilityState == AbilitySlot.AbilityState.READY) {
             cooldownForeground.sizeDelta = Vector2.zero;
+            
             cooldownText.text = "";
+            abilityKeyText.text = storedAbilityKeyText;
         }
 
         
