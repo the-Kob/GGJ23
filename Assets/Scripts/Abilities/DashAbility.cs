7b4d78d283b59ca318ff9349efa05caaeb5a0213
Paulo <pauliroti@gmail.com>
2023-02-05 01:02:55 +0000

    added vfx to abilties

--- Assets/Scripts/Abilities/DashAbility.cs
+++ Assets/Scripts/Abilities/DashAbility.cs
@@ -10,24 +10,33 @@ public class DashAbility : Ability {
     public float jumpBoostMult = 1;
     public float gravityMult = 1;
 
+    public GameObject vfx;
+
     private StarterAssets.ThirdPersonController _controller;
 
     public override void Activate(GameObject parent, InputAction action) {
         _controller = parent.GetComponent<StarterAssets.ThirdPersonController>();
 
-        RootsController.Instance.StartCoroutine(Dash());
+        RootsController.Instance.StartCoroutine(Dash(parent));
     }
 
-    IEnumerator Dash() {
+    IEnumerator Dash(GameObject parent) {
         _controller.MoveSpeed *= movSpeedMult;
         _controller.JumpHeight *= jumpBoostMult;
         _controller.Gravity *= gravityMult;
+
+        GameObject vfxBuffer = null;
+        if(vfx != null)
+            vfxBuffer = Instantiate(vfx, parent.transform);
 
         yield return new WaitForSeconds(duration);
 
         _controller.MoveSpeed = 5;
         _controller.JumpHeight = 1.2f;
         _controller.Gravity = -15;
+
+        if(vfxBuffer != null)
+            Destroy(vfxBuffer, 0.5f);
 
     }
 
